d19a287d06bc44ea3fb3e71e0ee600dcdeff699d
Vijaysid72201 <102346201+Vijaysid72201@users.noreply.github.com>
2022-06-03 14:11:42 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        for(int i=0;i<4;i++)
+        {
+            System.out.println("Hello World!");
+        }
     }
 }
